1d77339a561cf1cf17a0baca2f47d15d19edfe15
lzear <antoine.clausse@gmail.com>
2023-08-21 00:17:17 +0200

    Rewrite some test with `.each`

--- test/plugins/_index.test.ts
+++ test/plugins/_index.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import FS from 'node:fs'
 import { EOL } from 'node:os'
 import path from 'node:path'
@@ -10,51 +9,46 @@ const regEOL = new RegExp(EOL, 'g')
 const regFilename = /^(.*)\.(\d+)\.svg$/
 
 describe('plugins tests', function () {
-  FS.readdirSync(path.dirname(url.fileURLToPath(import.meta.url))).forEach(function (file) {
-    var match = file.match(regFilename),
-      index,
-      name
+  const files = FS.readdirSync(path.dirname(url.fileURLToPath(import.meta.url)))
 
+  it.each(files)('%s', function (file) {
+    const match = file.match(regFilename)
     if (match) {
-      name = match[1]
-      index = match[2]
+      const name = match[1]
 
       file = path.resolve(
         path.dirname(url.fileURLToPath(import.meta.url)),
-          file,
+        file,
       )
-
-      it(name + '.' + index, function () {
-        return readFile(file).then(function (data) {
-          // remove description
-          const items = normalize(data).split(/\s*===\s*/)
-          const test = items.length === 2 ? items[1] : items[0]
-          // extract test case
-          const [original, should, params] = test.split(/\s*@@@\s*/)
-          const plugin = {
-            name,
-            params: params ? JSON.parse(params) : {},
-          }
-          let lastResultData = original
-          // test plugins idempotence
-          const exclude = ['addAttributesToSVGElement', 'convertTransform']
-          const multipass = exclude.includes(name) ? 1 : 2
-          for (let i = 0; i < multipass; i += 1) {
-            const result = optimize(lastResultData, {
-              path: file,
-              plugins: [plugin],
-              js2svg: { pretty: true },
-            })
-            lastResultData = result.data
-            expect(result.error).not.toEqual(expect.anything())
-            //FIXME: results.data has a '\n' at the end while it should not
-            expect(normalize(result.data)).toEqual(should)
-          }
-        })
+      return readFile(file).then(function (data) {
+        // remove description
+        const items = normalize(data).split(/\s*===\s*/)
+        const test = items.length === 2 ? items[1] : items[0]
+        // extract test case
+        const [original, should, params] = test.split(/\s*@@@\s*/)
+        const plugin = {
+          name,
+          params: params ? JSON.parse(params) : {},
+        }
+        let lastResultData = original
+        // test plugins idempotence
+        const exclude = ['addAttributesToSVGElement', 'convertTransform']
+        const multipass = exclude.includes(name) ? 1 : 2
+        for (let i = 0; i < multipass; i += 1) {
+          const result = optimize(lastResultData, {
+            path: file,
+            plugins: [plugin],
+            js2svg: { pretty: true },
+          })
+          lastResultData = result.data
+          expect(result.error).not.toEqual(expect.anything())
+          //FIXME: results.data has a '\n' at the end while it should not
+          expect(normalize(result.data)).toEqual(should)
+        }
       })
     }
-  });
-});
+  })
+})
 
 function normalize(file) {
   return file.trim().replace(regEOL, '\n')
