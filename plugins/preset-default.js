6fca3ef6c141181b4b4039740df5b532115e2edc
lzear <antoine.clausse@gmail.com>
2024-11-17 21:20:13 +0100

    feat(applyTransformsShapes): add plugin (from KTibow/svgo)
    Co-authored-by: KTibow

--- plugins/preset-default.js
+++ plugins/preset-default.js
@@ -28,6 +28,7 @@ import * as moveGroupAttrsToElems from './moveGroupAttrsToElems.js';
 import * as collapseGroups from './collapseGroups.js';
 import * as convertPathData from './convertPathData.js';
 import * as convertTransform from './convertTransform.js';
+import * as applyTransformsShapes from './applyTransformsShapes.js';
 import * as removeEmptyAttrs from './removeEmptyAttrs.js';
 import * as removeEmptyContainers from './removeEmptyContainers.js';
 import * as mergePaths from './mergePaths.js';
@@ -65,6 +66,7 @@ const presetDefault = createPreset({
     moveGroupAttrsToElems,
     collapseGroups,
     convertPathData,
+    applyTransformsShapes,
     convertTransform,
     removeEmptyAttrs,
     removeEmptyContainers,
